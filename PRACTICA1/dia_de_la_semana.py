da0dc76d1f47174a6855eca152471295bd11baad
Hessel Humberto Valverde Moreno <hessel.valverde284143@potros.itson.edu.mx>
2025-10-30 18:33:44 -0600

    Asistente_del_dia

---
+++ PRACTICA1/dia_de_la_semana.py
@@ -0,0 +1,88 @@
+print("Dia de la semana")
+dia=input("""
+lunes
+martes
+miercoles
+jueves
+viernes
+sabado
+domingo
+""")
+print("Clima del dia")
+clima=input("""""
+soleado
+lluvioso
+nublado
+""")
+
+match dia:
+ case "lunes":
+   if clima == "soleado":
+     print("Es un gran día para estar en casa")
+   elif clima == "lluvioso":
+     print("Es un gran día para leer")
+   else:
+     print("Es un gran día para caminar")
+ 
+ case "martes":
+   if clima == "soleado":
+     print("Es un gran día para estar en casa")
+   elif clima == "lluvioso":
+     print("Es un gran día para leer")
+   else:
+     print("Es un gran día para caminar")
+ 
+ case "miercoles":
+   if clima == "soleado":
+     print("Es un gran día para estar en casa")
+   elif clima == "lluvioso":
+     print("Es un gran día para leer")
+   else:
+     print("Es un gran día para caminar")
+ 
+ case "jueves":
+   if clima == "soleado":
+     print("Es un gran día para estar en casa")
+   elif clima == "lluvioso":
+     print("Es un gran día para leer")
+   else:
+     print("Es un gran día para caminar")
+ 
+ case "viernes":
+   if clima == "soleado":
+     print("Es un gran día para estar en casa")
+   elif clima == "lluvioso":
+     print("Es un gran día para leer")
+   else:
+     print("Es un gran día para caminar")
+ 
+ case "sabado":
+   if clima == "soleado":
+     print("Es un gran día para estar en casa")
+   elif clima == "lluvioso":
+     print("Es un gran día para leer")
+   else:
+     print("Es un gran día para caminar")
+ 
+ case "domingo":
+   if clima == "soleado":
+     print("Es un gran día para estar en casa")
+   elif clima == "lluvioso":
+     print("Es un gran día para leer")
+   else:
+     print("Es un gran día para caminar")
+
+
+ 
+   
+  
+
+  
+ 
+ 
+    
+   
+   
+
+
+
